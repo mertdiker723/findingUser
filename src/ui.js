6e8e7c15b6623a72cac74a9bec583350b8dc6b39
Mert Diker <mertdiker723@gmail.com>
2019-05-08 15:27:14 +0300

    ui constructor verileri girildi

---
+++ src/ui.js
@@ -0,0 +1,8 @@
+export default class UI{
+    constructor(){
+        this.profileContainer = document.getElementById("profileContainer");
+        this.todo = document.getElementById("todo");
+        this.alert = document.getElementById("alert");
+        
+    }
+}
